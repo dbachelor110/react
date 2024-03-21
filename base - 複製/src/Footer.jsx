2ddd137408a4cc3ee9a829741bcd4b80239cf0ea
mmao rog <d44066041@gs.ncku.edu.tw>
2024-03-22 01:44:32 +0800

    makeup

--- base - 複製/src/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer(){
-    let footer = <footer>
-        <div>&copy; {new Date().getFullYear()} mmaoteacher</div>
-    </footer>
-    return footer
-}
-
-export default Footer
---
+++ base/src/Header.jsx
@@ -0,0 +1,14 @@
+function Header(){
+    let header = <header>
+        <h1>MMao React</h1>
+        <nav>
+            <ul>
+                <li>home</li>
+                <li>works</li>
+            </ul>
+        </nav>
+    </header>
+    return header
+}
+
+export default Header
